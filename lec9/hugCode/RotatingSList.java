cebe08d9324d7b37a2514db5e44f32a9de485f57
cs61b-buk <16856510+cs61b-buk@users.noreply.github.com>
2018-06-17 19:08:43 -0700

    Update RotatingSList.java

--- lec9/hugCode/RotatingSList.java
+++ lec9/hugCode/RotatingSList.java
@@ -1,8 +1,6 @@
 /* SList, but with additional rotateRight operation. */
 public class RotatingSList<Item> {
-
 	/** To do: Implement RotatingSList such that code compiles and outputs correct result. */
-
 	public static void main(String[] args) {
 		RotatingSList<Integer> rsl = new RotatingSList<Integer>();
 		/* Creates SList: [10, 11, 12, 13] */
@@ -10,7 +8,6 @@ public class RotatingSList<Item> {
 		rsl.insertBack(11);
 		rsl.insertBack(12);
 		rsl.insertBack(13);
-
 		/* Should be: [13, 10, 11, 12] */
 		rsl.rotateRight();
 		rsl.print();
